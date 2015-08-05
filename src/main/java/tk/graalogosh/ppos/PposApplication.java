1e9e490f16548e53ad19a2ac840d11a3808f5e86
graalogosh <graalogosh@gmail.com>
2015-08-06 02:32:40 +0300

    HTML init
    Added support for markup files

--- src/main/java/tk/graalogosh/ppos/PposApplication.java
+++ src/main/java/tk/graalogosh/ppos/PposApplication.java
@@ -2,8 +2,10 @@ package tk.graalogosh.ppos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class PposApplication {
 
     public static void main(String[] args) {
