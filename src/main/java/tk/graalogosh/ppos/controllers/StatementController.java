a849b896d032e35d0e36a43bedf8d6d69669abe6
graalogosh <graalogosh@gmail.com>
2015-09-11 15:28:42 +0300

    Statement search init

--- src/main/java/tk/graalogosh/ppos/controllers/StatementController.java
+++ src/main/java/tk/graalogosh/ppos/controllers/StatementController.java
@@ -1,6 +1,7 @@
 package tk.graalogosh.ppos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import tk.graalogosh.ppos.repositories.StatementRepository;
 import tk.graalogosh.ppos.repositories.StudentRepository;
 
 import javax.swing.plaf.nimbus.State;
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by graal on 05.08.2015.
@@ -24,9 +27,28 @@ public class StatementController {
     }
 
     @RequestMapping("statement")
-    public Statement getStudent(@RequestParam("statementID") Integer statementID){
-        Statement statement = statementRepository.findOne(statementID);
-       // System.out.println(student);
-        return statement;
+    public List<Statement> getStatements(
+            @RequestParam(value = "statementID", required = false) Integer statementID,
+            @RequestParam(value = "fillingDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate fillingDate,
+            @RequestParam(value = "studentID", required = false) String studentID,
+            @RequestParam(value = "eventID", required = false) int eventID,
+            @RequestParam(value = "employeeID", required = false) int employeeID,
+            @RequestParam(value = "socialGrant", required = false) boolean socialGrant,
+            @RequestParam(value = "socialCategoryID", required = false) int socialCategoryID,
+            @RequestParam(value = "socialWorkID", required = false) int socialWorkID,
+            @RequestParam(value = "moneyCategory", required = false) double moneyCategory,
+            @RequestParam(value = "cource", required = false) int cource,
+            @RequestParam(value = "tripCount", required = false) int tripCount,
+            @RequestParam(value = "averageScore", required = false) double averageScore,
+            @RequestParam(value = "refusalCount", required = false) int refusalCount,
+            @RequestParam(value = "permitNumber", required = false) int permitNumber,
+            @RequestParam(value = "refusalDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate refusalDate,
+            @RequestParam(value = "cancellationDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate cancellationDate,
+            @RequestParam(value = "listID", required = false) int listID,
+            @RequestParam(value = "comment", required = false) String comment,
+            @RequestParam(value = "completeDocs", required = false) boolean completeDocs,
+            @RequestParam(value = "reserve", required = false) boolean reserve){
+        Statement example = new Statement();
+        
     }
 }
--- src/main/java/tk/graalogosh/ppos/models/Statement.java
+++ src/main/java/tk/graalogosh/ppos/models/Statement.java
@@ -1,6 +1,8 @@
 package tk.graalogosh.ppos.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import tk.graalogosh.ppos.utils.LocalDateDeserializer;
 import tk.graalogosh.ppos.utils.LocalDatePersistenceConverter;
 
 import javax.persistence.*;
@@ -21,6 +23,7 @@ public class Statement {
 
     @Convert(converter = LocalDatePersistenceConverter.class)
     @JsonIgnoreProperties(value = {"dayOfWeek", "era", "dayOfYear", "leapYear", "chronology"})
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate fillingDate;
 
     @ManyToOne
@@ -66,10 +69,12 @@ public class Statement {
 
     @Convert(converter = LocalDatePersistenceConverter.class)
     @JsonIgnoreProperties(value = {"dayOfWeek", "era", "dayOfYear", "leapYear", "chronology"})
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate refusalDate;
 
     @Convert(converter = LocalDatePersistenceConverter.class)
     @JsonIgnoreProperties(value = {"dayOfWeek", "era", "dayOfYear", "leapYear", "chronology"})
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate cancellationDate;
 
     @ManyToOne
