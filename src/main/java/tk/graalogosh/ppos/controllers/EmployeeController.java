d71f410bfa022435f89cf1cb5ad30303e396be46
graalogosh <graalogosh@gmail.com>
2015-09-17 22:38:08 +0300

    EmployeeController init

--- src/main/java/tk/graalogosh/ppos/controllers/EmployeeController.java
+++ src/main/java/tk/graalogosh/ppos/controllers/EmployeeController.java
@@ -1,12 +1,14 @@
 package tk.graalogosh.ppos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tk.graalogosh.ppos.models.Employee;
 import tk.graalogosh.ppos.repositories.EmployeeRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -22,10 +24,15 @@ public class EmployeeController {
     }
 
     @RequestMapping("employee")
-    public Employee getEmployee(@RequestParam(value = "employeeID", defaultValue = "0") Integer employeeID) {
-        Employee employee = employeeRepository.findOne(employeeID);
-        System.out.println(employee);
-        //employeeRepository.sa
-        return employee;
+    public Employee getEmployees(
+            @RequestParam(value = "employeeID", defaultValue = "0") Integer employeeID,
+            @RequestParam(value = "name", required = false)String name,
+            @RequestParam(value = "accessLevel", required = false)int accessLevel,
+            @RequestParam(value = "registrationDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate registrationDate,
+            @RequestParam(value = "lastTimeLoginDate", required = false)@DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate lastTimeLoginDate,
+            @RequestParam(value = "dismissed", required = false) boolean dismissed,
+            @RequestParam(value = "faculty", required = false) String faculty) {
+
+        Employee example = new Employee();
     }
 }
