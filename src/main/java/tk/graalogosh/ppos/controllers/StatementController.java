761743c3a97d045eb8b1a9305818660d50c2706d
graalogosh <graalogosh@gmail.com>
2015-09-24 22:30:16 +0300

    Statement search init
    Will it work?
    ¯\_(ツ)_/¯

--- src/main/java/tk/graalogosh/ppos/controllers/StatementController.java
+++ src/main/java/tk/graalogosh/ppos/controllers/StatementController.java
@@ -3,14 +3,11 @@ package tk.graalogosh.ppos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
-import tk.graalogosh.ppos.models.Event;
-import tk.graalogosh.ppos.models.Statement;
-import tk.graalogosh.ppos.models.Student;
-import tk.graalogosh.ppos.repositories.CourseRepository;
-import tk.graalogosh.ppos.repositories.StatementRepository;
-import tk.graalogosh.ppos.repositories.StudentRepository;
+import tk.graalogosh.ppos.models.*;
+import tk.graalogosh.ppos.repositories.*;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import tk.graalogosh.ppos.specifications.StatementSpecification;
 
 import javax.swing.plaf.nimbus.State;
 import java.time.LocalDate;
@@ -24,14 +21,35 @@ public class StatementController {
     private StatementRepository statementRepository;
     private StudentRepository studentRepository;
     private CourseRepository courseRepository;
+    private EventRepository eventRepository;
+    private EmployeeRepository employeeRepository;
+    private SocialCategoryRepository socialCategoryRepository;
+    private SocialWorkRepository socialWorkRepository;
+    private TripCountRepository tripCountRepository;
+    private RefusalRepository refusalRepository;
+    private StatementListRepository statementListRepository;
 
     @Autowired
     public StatementController(StatementRepository statementRepository,
                                StudentRepository studentRepository,
-                               CourseRepository courseRepository){
+                               CourseRepository courseRepository,
+                               EventRepository eventRepository,
+                               EmployeeRepository employeeRepository,
+                               SocialCategoryRepository socialCategoryRepository,
+                               SocialWorkRepository socialWorkRepository,
+                               TripCountRepository tripCountRepository,
+                               RefusalRepository refusalRepository,
+                               StatementListRepository statementListRepository){
         this.statementRepository = statementRepository;
         this.studentRepository = studentRepository;
         this.courseRepository = courseRepository;
+        this.eventRepository = eventRepository;
+        this.employeeRepository = employeeRepository;
+        this.socialCategoryRepository = socialCategoryRepository;
+        this.socialWorkRepository = socialWorkRepository;
+        this.tripCountRepository = tripCountRepository;
+        this.refusalRepository = refusalRepository;
+        this.statementListRepository = statementListRepository;
     }
 
     @RequestMapping("statement")
@@ -39,36 +57,48 @@ public class StatementController {
             @RequestParam(value = "statementID", required = false) Integer statementID,
             @RequestParam(value = "fillingDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate fillingDate,
             @RequestParam(value = "studentID", required = false) String studentID,
-            @RequestParam(value = "eventID", required = false) int eventID,
-            @RequestParam(value = "employeeID", required = false) int employeeID,
-            @RequestParam(value = "socialGrant", required = false) boolean socialGrant,
-            @RequestParam(value = "socialCategoryID", required = false) int socialCategoryID,
-            @RequestParam(value = "socialWorkID", required = false) int socialWorkID,
-            @RequestParam(value = "moneyCategory", required = false) double moneyCategory,
-            @RequestParam(value = "cource", required = false) int cource,
-            @RequestParam(value = "tripCount", required = false) int tripCount,
-            @RequestParam(value = "averageScore", required = false) double averageScore,
-            @RequestParam(value = "refusalCount", required = false) int refusalCount,
-            @RequestParam(value = "permitNumber", required = false) int permitNumber,
+            @RequestParam(value = "eventID", required = false) Integer eventID,
+            @RequestParam(value = "employeeID", required = false) Integer employeeID,
+            @RequestParam(value = "socialGrant", required = false) Boolean socialGrant,
+            @RequestParam(value = "socialCategoryID", required = false) Integer socialCategoryID,
+            @RequestParam(value = "socialWorkID", required = false) Integer socialWorkID,
+            @RequestParam(value = "moneyCategory", required = false) Double moneyCategory,
+            @RequestParam(value = "cource", required = false) Integer cource,
+            @RequestParam(value = "tripCount", required = false) Integer tripCount,
+            @RequestParam(value = "averageScore", required = false) Double averageScore,
+            @RequestParam(value = "refusalCount", required = false) Integer refusalCount,
+            @RequestParam(value = "permitNumber", required = false) Integer permitNumber,
             @RequestParam(value = "refusalDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate refusalDate,
             @RequestParam(value = "cancellationDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate cancellationDate,
-            @RequestParam(value = "listID", required = false) int listID,
+            @RequestParam(value = "listID", required = false) Integer listID,
             @RequestParam(value = "comment", required = false) String comment,
-            @RequestParam(value = "completeDocs", required = false) boolean completeDocs,
-            @RequestParam(value = "reserve", required = false) boolean reserve){
+            @RequestParam(value = "completeDocs", required = false) Boolean completeDocs,
+            @RequestParam(value = "reserve", required = false) Boolean reserve){
         Statement example = new Statement();
         example.setStatementID(statementID);
         example.setFillingDate(fillingDate);
-        Student student=studentRepository.findOne(studentID);
-        if (student!=null) {
-            example.setStudent(student);
-        }
-        else{
-            example.setStudent(new Student());//null
-        }
-//        Event event =
-//        example.setEvent();
-        throw new NotImplementedException();
+        example.setStudent(studentRepository.findOne(studentID));
+        example.setEvent(eventRepository.findOne(eventID));
+        example.setEmployee(employeeRepository.findOne(employeeID));
+        example.setSocialGrant(socialGrant);
+        example.setSocialCategory(socialCategoryRepository.findOne(socialCategoryID));
+        example.setSocialWork(socialWorkRepository.findOne(socialWorkID));
+        example.setMoneyCategory(moneyCategory);
+        example.setCourse(courseRepository.findOne(cource));
+        example.setTripCount(tripCountRepository.findOne(tripCount));
+        example.setAverage_score(averageScore);
+        example.setRefusalCount(refusalRepository.findOne(refusalCount));
+        example.setPermitNumber(permitNumber);
+        example.setRefusalDate(refusalDate);
+        example.setCancellationDate(cancellationDate);
+        example.setList(statementListRepository.findOne(listID));
+        example.setComment(comment);
+        example.setCompleteDocs(completeDocs);
+        example.setReserve(reserve);
+
+        StatementSpecification specification = new StatementSpecification(example);
+        List<Statement> statements = statementRepository.findAll(specification);
+        return statements;
     }
 
     @RequestMapping(value = "statement", method = RequestMethod.POST)
--- src/main/java/tk/graalogosh/ppos/specifications/StatementSpecification.java
+++ src/main/java/tk/graalogosh/ppos/specifications/StatementSpecification.java
@@ -1,7 +1,113 @@
 package tk.graalogosh.ppos.specifications;
 
+import org.springframework.data.jpa.domain.Specification;
+import tk.graalogosh.ppos.models.Statement;
+import tk.graalogosh.ppos.models.Statement_;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by graal on 11.09.2015.
  */
-public class StatementSpecification {
+public class StatementSpecification implements Specification<Statement> {
+    private Statement example;
+
+    public StatementSpecification(Statement example) {
+        this.example = example;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Statement> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (example.getStatementID() != null) {
+            predicates.add(cb.equal(root.get(Statement_.statementID), example.getStatementID()));
+        }
+
+        if (example.getFillingDate() != null) {
+            predicates.add(cb.equal(root.get(Statement_.fillingDate), example.getFillingDate()));
+        }
+
+        if (example.getStudent() != null) {
+            predicates.add(cb.equal(root.get(Statement_.student), example.getStudent()));
+        }
+
+        if (example.getEvent() != null) {
+            predicates.add(cb.equal(root.get(Statement_.event), example.getEvent()));
+        }
+
+        if (example.getEmployee() != null) {
+            predicates.add(cb.equal(root.get(Statement_.employee), example.getEmployee()));
+        }
+
+        if (example.getSocialGrant() != null) {
+            predicates.add(cb.equal(root.get(Statement_.socialGrant), example.getSocialCategory()));
+        }
+
+        if (example.getSocialCategory() != null) {
+            predicates.add(cb.equal(root.get(Statement_.socialCategory), example.getSocialCategory()));
+        }
+
+        if (example.getSocialWork() != null) {
+            predicates.add(cb.equal(root.get(Statement_.socialWork), example.getSocialWork()));
+        }
+
+        if (example.getMoneyCategory() != null) {
+            predicates.add(cb.equal(root.get(Statement_.moneyCategory), example.getMoneyCategory()));
+        }
+
+        if (example.getCourse() != null) {
+            predicates.add(cb.equal(root.get(Statement_.course), example.getCourse()));
+        }
+
+        if (example.getTripCount() != null) {
+            predicates.add(cb.equal(root.get(Statement_.tripCount), example.getTripCount()));
+        }
+        //TODO fix score
+        //if (example.getAverage_score()){}
+
+        if (example.getRefusalCount() != null) {
+            predicates.add(cb.equal(root.get(Statement_.refusalCount), example.getRefusalCount()));
+        }
+
+        if (example.getPermitNumber() != null) {
+            predicates.add(cb.equal(root.get(Statement_.permitNumber), example.getPermitNumber()));
+        }
+
+        if (example.getRefusalDate() != null) {
+            predicates.add(cb.equal(root.get(Statement_.refusalDate), example.getRefusalDate()));
+        }
+
+        if (example.getCancellationDate() != null) {
+            predicates.add(cb.equal(root.get(Statement_.cancellationDate), example.getCancellationDate()));
+        }
+
+        if (example.getList() != null) {
+            predicates.add(cb.equal(root.get(Statement_.list), example.getList()));
+        }
+
+        //TODO fix comment search
+        if (example.getComment() != null) {
+            predicates.add(cb.equal(root.get(Statement_.comment), example.getComment()));
+        }
+
+        if (example.getCompleteDocs() != null) {
+            predicates.add(cb.equal(root.get(Statement_.completeDocs), example.getCompleteDocs()));
+        }
+
+        if (example.getReserve() != null) {
+            predicates.add(cb.equal(root.get(Statement_.reserve), example.getReserve()));
+        }
+
+        return andTogether(predicates, cb);
+    }
+
+    private Predicate andTogether(List<Predicate> predicates, CriteriaBuilder cb) {
+        return cb.and(predicates.toArray(new Predicate[0]));
+    }
 }
