0dda68bd144a8331eb406fb93bf831c161e2df7c
graalogosh <graalogosh@gmail.com>
2015-09-24 19:37:25 +0300

    RefusalFix 2

--- src/main/java/tk/graalogosh/ppos/repositories/RefusalRepository.java
+++ src/main/java/tk/graalogosh/ppos/repositories/RefusalRepository.java
@@ -2,7 +2,7 @@ package tk.graalogosh.ppos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import tk.graalogosh.ppos.models.Course;
+import tk.graalogosh.ppos.models.Refusal;
 
 import javax.persistence.Table;
 
