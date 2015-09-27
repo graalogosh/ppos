88e31961189a28796f45145257d3864de0c07de9
yakun74 <yakun-74@mail.ru>
2015-09-27 20:24:07 +0300

    Науйчил функцию sendInform работать с checkbox'ами

--- src/main/resources/static/js/function_sendInform.js
+++ src/main/resources/static/js/function_sendInform.js
@@ -4,9 +4,17 @@ function sendInform () {
 	var object = {};
 
 	//формируем поля объекта в соответствии с переданными параметрами 
-	//(переданный параметр - имя поля, значение поля - содержимое элемента с таким именем)
+	//(переданный параметр - имя поля, значение поля - содержимое value выбранного элемента)
 	for (var i = 0; i < arguments.length; i++) {
-		object[arguments[i]] = $("#" + arguments[i]).val();
+		//проверяет, не является ли элемент checkbox'ом. Если не является, то записываем value выбранного элемента
+		if ($("#" + arguments[i]).attr("type") != "checkbox")
+		{
+			object[arguments[i]] = $("#" + arguments[i]).val();
+		}
+		//если элемент является checkbox'ом, то записываем в содержимое поля, нажат он или нет
+		else {
+			object[arguments[i]] = $("#" + arguments[i]).is(":checked");
+		}
 	}
 	return object;
 }
