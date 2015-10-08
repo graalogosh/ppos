c38dd8f031e839ea7f02bcff919a15a6e2035658
graalogosh <graalogosh@gmail.com>
2015-10-08 23:19:45 +0300

    FacultyController init + Faculty model fix

---
+++ src/main/java/tk/graalogosh/ppos/controllers/FacultyController.java
@@ -0,0 +1,37 @@
+package tk.graalogosh.ppos.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import tk.graalogosh.ppos.models.Faculty;
+import tk.graalogosh.ppos.repositories.FacultyRepository;
+
+import java.util.List;
+
+/**
+ * Created by graal on 08.10.2015.
+ */
+@RestController
+@RequestMapping(value = "faculties")
+public class FacultyController {
+    private FacultyRepository facultyRepository;
+
+    @Autowired
+    public FacultyController(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Faculty> getFaculties(){
+        return facultyRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Boolean postFaculty(
+            @RequestBody Faculty payload){
+        throw new NotImplementedException();
+    }
+}
