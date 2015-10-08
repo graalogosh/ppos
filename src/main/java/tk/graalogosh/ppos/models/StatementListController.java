464d73dad3342482c107059dd78b7200f3c955a6
graalogosh <graalogosh@gmail.com>
2015-10-09 01:09:41 +0300

    StatementListController init

---
+++ src/main/java/tk/graalogosh/ppos/models/StatementListController.java
@@ -0,0 +1,35 @@
+package tk.graalogosh.ppos.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import tk.graalogosh.ppos.repositories.StatementListRepository;
+
+import java.util.List;
+
+/**
+ * Created by graal on 09.10.2015.
+ */
+@RestController
+@RequestMapping(value = "statementList")
+public class StatementListController {
+    private StatementListRepository statementListRepository;
+
+    @Autowired
+    public StatementListController(StatementListRepository statementListRepository) {
+        this.statementListRepository = statementListRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<StatementList> getStatementLists(){
+        return statementListRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Boolean postStatementList(
+           @RequestBody StatementList payload){
+        throw new UnsupportedOperationException();
+    }
+}
