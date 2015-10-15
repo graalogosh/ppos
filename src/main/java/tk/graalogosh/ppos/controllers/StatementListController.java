3129b996f942517b9b55423056ab5cfc3b99da19
graalogosh <graalogosh@gmail.com>
2015-10-15 12:33:18 +0300

    StatementListController package fix

--- src/main/java/tk/graalogosh/ppos/controllers/StatementListController.java
+++ src/main/java/tk/graalogosh/ppos/controllers/StatementListController.java
@@ -1,10 +1,11 @@
-package tk.graalogosh.ppos.models;
+package tk.graalogosh.ppos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import tk.graalogosh.ppos.models.StatementList;
 import tk.graalogosh.ppos.repositories.StatementListRepository;
 
 import java.util.List;
