55afb31fd1492b2c99ed5fc54f3a5666dbd77a22
graalogosh <graalogosh@gmail.com>
2015-10-22 08:25:24 +0300

    Specification mechanism for EventController

--- src/main/java/tk/graalogosh/ppos/controllers/EventController.java
+++ src/main/java/tk/graalogosh/ppos/controllers/EventController.java
@@ -1,6 +1,8 @@
 package tk.graalogosh.ppos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import tk.graalogosh.ppos.models.Event;
@@ -33,6 +35,56 @@ public class EventController {
         this.sectionRepository = sectionRepository;
     }
 
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Event> getEvents(
+//            @RequestParam(value = "eventID", required = false) Integer eventID,
+//            @RequestParam(value = "title", required = false) String title,
+//            @RequestParam(value = "eventDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate eventDate,
+//            @RequestParam(value = "duration", required = false) Integer duration,
+//            @RequestParam(value = "reseptionBegin", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate receptionBegin,
+//            @RequestParam(value = "reseptionFinish", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate reseptionFinish,
+//            @RequestParam(value = "employeeID", required = false) Integer employeeID,
+//            @RequestParam(value = "sectionID", required = false) Integer sectionID,
+//            @RequestParam(value = "numberOfPlaces", required = false) Integer numberOfPlaces,
+//            @RequestParam(value = "quotasPercentage", required = false) Integer quotasPercentage,
+//            //TODO suitableCategory
+//            @RequestParam(value = "suitableCategory", required = false) String suitableCategory,
+//            @RequestParam(value = "firstDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate firstDate,
+//            @RequestParam(value = "lastDate", required = false) @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate lastDate,
+//            @RequestParam(value = "curTerm", required = false) Boolean curTerm) {
+//
+//        firstDate = firstDate != null ? firstDate : Dates.MINDATE;
+//        lastDate = lastDate != null ? lastDate : Dates.MAXDATE;
+//        curTerm = curTerm != null ? curTerm : true;
+//
+//        Event example = new Event();
+//        example.setEventID(eventID);
+//        example.setTitle(title);
+//        example.setEventDate(eventDate);
+//        example.setDuration(duration);
+//        example.setReseptionBegin(receptionBegin);
+//        example.setReseptionFinish(reseptionFinish);
+//        example.setEmployee(employeeID != null ? employeeRepository.findOne(employeeID) : null);
+//        example.setSection(sectionID != null ? sectionRepository.findOne(sectionID) : null);
+//        example.setNumberOfPlaces(numberOfPlaces);
+//        example.setQuotasPercantage(quotasPercentage);
+//        example.setSuitableCategory(suitableCategory);
+//
+//        EventSpecification specification = new EventSpecification(example);
+//        List<Event> events1 = eventRepository.findAll(specification);
+//
+//        List<Event> events2 = eventRepository.findByEventDateBetween(firstDate, lastDate);
+//        List<Event> events3 = eventRepository.findByReseptionBeginBeforeAndReseptionFinishAfter(LocalDate.now());
+//        List<Event> resultEvents = new ArrayList<>();
+//        for (Event event : events1) {
+//            //пересечение event, event2 и event3 (только если curTerm=true)
+//            if (events2.contains(event) && (events3.contains(event) || !curTerm)) {
+//                resultEvents.add(event);
+//            }
+//        }
+//        return resultEvents;
+//    }
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Event> getEvents(
             @RequestParam(value = "eventID", required = false) Integer eventID,
@@ -55,32 +107,55 @@ public class EventController {
         lastDate = lastDate != null ? lastDate : Dates.MAXDATE;
         curTerm = curTerm != null ? curTerm : true;
 
-        Event example = new Event();
-        example.setEventID(eventID);
-        example.setTitle(title);
-        example.setEventDate(eventDate);
-        example.setDuration(duration);
-        example.setReseptionBegin(receptionBegin);
-        example.setReseptionFinish(reseptionFinish);
-        example.setEmployee(employeeID != null ? employeeRepository.findOne(employeeID) : null);
-        example.setSection(sectionID != null ? sectionRepository.findOne(sectionID) : null);
-        example.setNumberOfPlaces(numberOfPlaces);
-        example.setQuotasPercantage(quotasPercentage);
-        example.setSuitableCategory(suitableCategory);
-
-        EventSpecification specification = new EventSpecification(example);
-        List<Event> events1 = eventRepository.findAll(specification);
-
-        List<Event> events2 = eventRepository.findByEventDateBetween(firstDate, lastDate);
-        List<Event> events3 = eventRepository.findByReseptionBeginBeforeAndReseptionFinishAfter(LocalDate.now());
-        List<Event> resultEvents = new ArrayList<>();
-        for (Event event : events1) {
-            //пересечение event, event2 и event3 (только если curTerm=true)
-            if (events2.contains(event) && (events3.contains(event) || !curTerm)) {
-                resultEvents.add(event);
-            }
+        List<Specification<Event>> specifications = new ArrayList<>();
+        if (eventID != null){
+            specifications.add(EventSpecification.IDIs(eventID));
         }
-        return resultEvents;
+
+        if (title!=null){
+            specifications.add(EventSpecification.titleIs(title));
+        }
+
+        if (eventDate!=null){
+            specifications.add(EventSpecification.eventDateIs(eventDate));
+        }
+
+        if (duration!=null){
+            specifications.add(EventSpecification.durationIs(duration));
+        }
+
+        if (receptionBegin!=null){
+            specifications.add(EventSpecification.reseptionBeginIn(receptionBegin));
+        }
+
+        if (reseptionFinish!=null){
+            specifications.add(EventSpecification.reseptionFinishIn(reseptionFinish));
+        }
+
+        if (employeeID!=null){
+            specifications.add(EventSpecification.employeeIs(employeeRepository.findOne(employeeID)));
+        }
+
+        if (sectionID!=null){
+            specifications.add(EventSpecification.sectionIs(sectionRepository.findOne(sectionID)));
+        }
+
+        if (numberOfPlaces!=null){
+            specifications.add(EventSpecification.numberOfPlacesIs(numberOfPlaces));
+        }
+
+        if (quotasPercentage!=null){
+            specifications.add(EventSpecification.quotasPercantageIs(quotasPercentage));
+        }
+
+        //TODO fisrtDate-lastDate
+        //TODO curTerm
+
+        Specification<Event> finalSpecification=null;
+        for (Specification<Event> spec : specifications){
+            finalSpecification = Specifications.where(finalSpecification).and(spec);
+        }
+        return eventRepository.findAll(finalSpecification);
     }
 
     @RequestMapping(method = RequestMethod.POST)
--- src/main/java/tk/graalogosh/ppos/specifications/EventSpecification.java
+++ src/main/java/tk/graalogosh/ppos/specifications/EventSpecification.java
@@ -1,78 +1,179 @@
 package tk.graalogosh.ppos.specifications;
 
 import org.springframework.data.jpa.domain.Specification;
+import tk.graalogosh.ppos.models.Employee;
 import tk.graalogosh.ppos.models.Event;
 import tk.graalogosh.ppos.models.Event_;
+import tk.graalogosh.ppos.models.Section;
 
+import javax.naming.OperationNotSupportedException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by graal on 01.10.2015.
  */
-public class EventSpecification implements Specification<Event> {
-    private Event example;
+//public class EventSpecification implements Specification<Event> {
+//    private Event example;
+//
+//    public EventSpecification(Event example) {
+//        this.example = example;
+//    }
+//
+//    @Override
+//    public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+//        List<Predicate> predicates = new ArrayList<>();
+//
+//        if (example.getEventID()!=null){
+//           predicates.add(cb.equal(root.get(Event_.eventID), example.getEventID()));
+//        }
+//
+//        if  (example.getTitle()!=null){
+//            predicates.add(cb.equal(root.get(Event_.title), example.getTitle()));
+//        }
+//
+//        if (example.getEventDate()!=null){
+//            predicates.add(cb.equal(root.get(Event_.eventDate),example.getEventDate()));
+//        }
+//
+//        if (example.getDuration()!=null){
+//            predicates.add(cb.equal(root.get(Event_.duration), example.getDuration()));
+//        }
+//
+//        if (example.getReseptionBegin()!=null){
+//            predicates.add(cb.equal(root.get(Event_.reseptionBegin), example.getReseptionBegin()));
+//        }
+//
+//        if (example.getReseptionFinish()!=null){
+//            predicates.add(cb.equal(root.get(Event_.reseptionFinish), example.getReseptionFinish()));
+//        }
+//
+//        if (example.getEmployee()!=null){
+//            predicates.add(cb.equal(root.get(Event_.employee), example.getEmployee()));
+//        }
+//
+//        if (example.getSection()!=null){
+//            predicates.add(cb.equal(root.get(Event_.section), example.getSection()));
+//        }
+//
+//        if (example.getNumberOfPlaces()!=null){
+//            predicates.add(cb.equal(root.get(Event_.numberOfPlaces), example.getNumberOfPlaces()));
+//        }
+//
+//        if (example.getQuotasPercantage()!=null){
+//            predicates.add(cb.equal(root.get(Event_.quotasPercantage), example.getQuotasPercantage()));
+//        }
+//
+//        if (example.getSuitableCategory()!=null){
+//            predicates.add(cb.like(root.get(Event_.suitableCategory), example.getSuitableCategory()));
+//        }
+//
+//        return andTogether(predicates, cb);
+//    }
+//
+//    private Predicate andTogether(List<Predicate> predicates, CriteriaBuilder cb) {
+//        return cb.and(predicates.toArray(new Predicate[0]));
+//    }
+//}
 
-    public EventSpecification(Event example) {
-        this.example = example;
+public class EventSpecification {
+    public static Specification<Event> IDIs(Integer eventID){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get((Event_.eventID)),eventID);
+            }
+        };
     }
 
-    @Override
-    public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-        List<Predicate> predicates = new ArrayList<>();
-
-        if (example.getEventID()!=null){
-           predicates.add(cb.equal(root.get(Event_.eventID), example.getEventID()));
-        }
-
-        if  (example.getTitle()!=null){
-            predicates.add(cb.equal(root.get(Event_.title), example.getTitle()));
-        }
-
-        if (example.getEventDate()!=null){
-            predicates.add(cb.equal(root.get(Event_.eventDate),example.getEventDate()));
-        }
-
-        if (example.getDuration()!=null){
-            predicates.add(cb.equal(root.get(Event_.duration), example.getDuration()));
-        }
-
-        if (example.getReseptionBegin()!=null){
-            predicates.add(cb.equal(root.get(Event_.reseptionBegin), example.getReseptionBegin()));
-        }
-
-        if (example.getReseptionFinish()!=null){
-            predicates.add(cb.equal(root.get(Event_.reseptionFinish), example.getReseptionFinish()));
-        }
-
-        if (example.getEmployee()!=null){
-            predicates.add(cb.equal(root.get(Event_.employee), example.getEmployee()));
-        }
-
-        if (example.getSection()!=null){
-            predicates.add(cb.equal(root.get(Event_.section), example.getSection()));
-        }
-
-        if (example.getNumberOfPlaces()!=null){
-            predicates.add(cb.equal(root.get(Event_.numberOfPlaces), example.getNumberOfPlaces()));
-        }
-
-        if (example.getQuotasPercantage()!=null){
-            predicates.add(cb.equal(root.get(Event_.quotasPercantage), example.getQuotasPercantage()));
-        }
-
-        if (example.getSuitableCategory()!=null){
-            predicates.add(cb.like(root.get(Event_.suitableCategory), example.getSuitableCategory()));
-        }
-
-        return andTogether(predicates, cb);
+    public static Specification<Event> titleIs(String title){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.title), title);
+            }
+        };
     }
 
-    private Predicate andTogether(List<Predicate> predicates, CriteriaBuilder cb) {
-        return cb.and(predicates.toArray(new Predicate[0]));
+    public static Specification<Event> eventDateIs(LocalDate date){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.eventDate),date);
+            }
+        };
+    }
+
+    public static Specification<Event> durationIs(Integer duration){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.duration), duration);
+            }
+        };
+    }
+
+    public static Specification<Event> reseptionBeginIn(LocalDate date){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.reseptionBegin), date);
+            }
+        };
+    }
+
+    public static Specification<Event> reseptionFinishIn(LocalDate date){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.reseptionFinish), date);
+            }
+        };
+    }
+
+    public static Specification<Event> employeeIs (Employee employee){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.employee), employee);
+            }
+        };
+    }
+
+    public static Specification<Event> sectionIs (Section section){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.section), section);
+            }
+        };
+    }
+
+    public static Specification<Event> numberOfPlacesIs (Integer number){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.numberOfPlaces), number);
+            }
+        };
+    }
+
+    public static Specification<Event> quotasPercantageIs (Integer quotas){
+        return new Specification<Event>() {
+            @Override
+            public Predicate toPredicate(Root<Event> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.equal(root.get(Event_.quotasPercantage),quotas);
+            }
+        };
+    }
+
+    //TODO fix category
+    public static Specification<Event> suitableCategoryIs (String category) throws OperationNotSupportedException {
+        throw new OperationNotSupportedException();
     }
 }
